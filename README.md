91fe53de4919866f1ecd6a8ca78ba390fc4f35dc
Wahib <nurwahibzakkiy@gmail.com>
2025-10-14 10:35:48 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# jawara_pintar_mobile_version
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/jawara_pintar_mobile_version/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.jawara_pintar_mobile_version
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
