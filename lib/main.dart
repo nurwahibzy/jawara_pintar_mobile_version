70f05cd92947f207a8ca6d878c66d357b9b34bc8
latif <mabdullatifm1127@gmail.com>
2025-10-23 10:30:23 +0700

    Tambah Data, Channel Transfer, Laporan Keuangan, Mutasi Keluarga

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,11 @@ import 'package:jawara_pintar_mobile_version/pages/warga/edit_warga.dart';
 import 'package:jawara_pintar_mobile_version/pages/pengeluaran/daftar_pengeluaran.dart';
 import 'package:jawara_pintar_mobile_version/pages/pengeluaran/tambah_pengeluaran.dart';
 import 'package:jawara_pintar_mobile_version/pages/pengeluaran/edit_pengeluaran.dart';
+import 'package:jawara_pintar_mobile_version/pages/Laporan/channel_transfer.dart';
+import 'package:jawara_pintar_mobile_version/pages/Laporan/LaporanKeuangan.dart';
+import 'package:jawara_pintar_mobile_version/pages/data_rumah_warga/mutasi_keluarga.dart';
+import 'package:jawara_pintar_mobile_version/pages/data_rumah_warga/penerimaan_warga_page.dart';
+
 
 void main() {
   runApp(const MainApp());
@@ -44,6 +49,10 @@ class MainApp extends StatelessWidget {
         '/daftar_pengeluaran': (context) => const DaftarPengeluaran(),
         '/tambah_pengeluaran': (context) => const TambahPengeluaran(),
         '/edit_pengeluaran': (context) => const EditPengeluaranPage(pengeluaran: {}),
+        // '/channel_transfer': (context) => const ChannelTransfer(),
+        '/laporan_keuangan': (context) => const LaporanKeuangan(),
+        // '/mutasi_keluarga': (context) => const MutasiKeluarga(),
+        '/penerimaan_warga': (context) => const PenerimaanWargaPage(),
 
       },
     );
