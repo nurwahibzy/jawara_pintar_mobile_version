e420641e886e97dcb0a98a3fe3de7d4bb8ed585d
Nurwahib Zakki Yahya <144310454+nurwahibzy@users.noreply.github.com>
2025-11-19 16:08:57 +0700

    Enhance app structure with routing and theming (#31)
    * Update .gitignore, pubspec.yaml, and pubspec.lock to include .env and new dependencies
    
    * Add routing, theming, and error handling components
    
    * Refactor main.dart to initialize Supabase and improve error handling

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xff8E6CEF);
+  static const background = Color.fromARGB(255, 173, 151, 229);
+  static const secondBackground = Color.fromARGB(255, 202, 201, 202);
+}
