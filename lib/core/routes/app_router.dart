39512afd6f03a36d923b0d44c6f0685b81515367
Nurwahib Zakki Yahya <144310454+nurwahibzy@users.noreply.github.com>
2025-11-26 21:01:06 +0700

    Revert "Add Daftar Mutasi Keluarga page with routing and error handling (#38)"
    This reverts commit 2c7e5bf4492bfd66e1a64ac2b0dd7c118f826a27.

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,16 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:jawara_pintar_mobile_version/features/mutasi-keluarga/presentation/pages/tambah_mutasi_keluarga.dart';
-import 'app_routes.dart';
-// import halaman yg dibutuhkan
-import 'package:jawara_pintar_mobile_version/pages/login/login_page.dart';
-import 'package:jawara_pintar_mobile_version/features/mutasi-keluarga/injection.dart';
-import 'package:jawara_pintar_mobile_version/features/mutasi-keluarga/presentation/bloc/mutasi_keluarga_bloc.dart';
-import 'package:jawara_pintar_mobile_version/features/mutasi-keluarga/presentation/pages/daftar_mutasi_keluarga.dart';
-
-class AppRouter {
-  static Route<dynamic> generateRoute(RouteSettings settings) {
-    // Mengambil arguments (jika ada)
 import 'package:jawara_pintar_mobile_version/core/injections/injection.dart';
 import 'package:jawara_pintar_mobile_version/features/pengeluaran/domain/entities/pengeluaran.dart';
 import 'package:jawara_pintar_mobile_version/features/pengeluaran/presentation/bloc/pengeluaran_bloc.dart';
@@ -32,22 +21,6 @@ class AppRouter {
     switch (settings.name) {
       case AppRoutes.login:
         return MaterialPageRoute(builder: (_) => const LoginPage());
-
-      case AppRoutes.daftarMutasiKeluarga:
-        return MaterialPageRoute(
-          builder: (_) => BlocProvider(
-            create: (context) => myInjection<MutasiKeluargaBloc>(),
-            child: const DaftarMutasiKeluarga(),
-          ),
-        );
-
-      case AppRoutes.tambahMutasiKeluarga:
-      return MaterialPageRoute(
-        builder: (_) => BlocProvider(
-          create: (context) => myInjection<MutasiKeluargaBloc>(),
-          child: const TambahMutasiKeluarga(),
-        ),
-      );
   //pengeluaran
   case AppRoutes.daftarPengeluaran:
     return MaterialPageRoute(
@@ -86,7 +59,6 @@ class AppRouter {
     }
   }
 
-  // Halaman Error jika route tidak ditemukan
   static Route<dynamic> _errorRoute() {
     return MaterialPageRoute(
       builder: (_) {
